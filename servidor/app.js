7529dde52354129666c92f0056c000ce18378046
Zetzher <j.abasolo.agenjo@gmail.com>
2020-06-25 14:29:24 +0200

    server done

--- servidor/app.js
+++ servidor/app.js
@@ -55,14 +55,11 @@ app.use(
 );
 
 
-   app.use((req, res, next) => {
-    
-   res.header('Access-Control-Allow-Origin', '*');
-   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST OPTIONS, DELETE');
-   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-   res.header('Access-Control-Allow-Credentials', true);
-   next();
- });
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+});
 
 
 app.use(logger('dev'));
