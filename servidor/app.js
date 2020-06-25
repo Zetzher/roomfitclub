6915a39fde8759f56ad5a19c339fa8ca5b092d26
Zetzher <j.abasolo.agenjo@gmail.com>
2020-06-25 13:39:51 +0200

    server done

--- servidor/app.js
+++ servidor/app.js
@@ -49,16 +49,20 @@ app.use(
 app.use(
   cors({
     credentials: true,
-    origin: ['http://localhost:3000', 'http://roomfitclub.com/'],
+    origin: ['http://localhost:3000', 'http://roomfitclub.com/', 'https://roomfit.herokuapp.com'],
   })
 );
-//   app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST OPTIONS, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   next();
-// });
+
+
+  /* app.use((req, res, next) => {
+   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000', "http://roomfitclub.com/");
+   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST OPTIONS, DELETE');
+   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+   res.setHeader('Access-Control-Allow-Credentials', true);
+   next();
+ });*/
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
