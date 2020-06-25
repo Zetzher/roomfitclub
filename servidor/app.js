8f06f5d68a12f4accb9dad798a4e13836d1957d5
Zetzher <j.abasolo.agenjo@gmail.com>
2020-06-25 17:26:15 +0200

    server done

--- servidor/app.js
+++ servidor/app.js
@@ -50,13 +50,13 @@ app.use(
 app.use(
   cors({
     credentials: true,
-    origin: ['*'],
+    origin: ['http://localhost:3000/'],
   })
 );
 
 
 app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next()
 });
