66309167d00bab6a31312ea3bc8a4732b782892e
Zetzher <j.abasolo.agenjo@gmail.com>
2020-06-25 14:09:54 +0200

    server done

--- servidor/app.js
+++ servidor/app.js
@@ -55,7 +55,8 @@ app.use(
 
 
    app.use((req, res, next) => {
-   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000', "http://roomfitclub.com/", 'https://roomfitclub.com/');
+    
+   res.setHeader('Access-Control-Allow-Origin', '*');
    res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST OPTIONS, DELETE');
    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
    res.setHeader('Access-Control-Allow-Credentials', true);
