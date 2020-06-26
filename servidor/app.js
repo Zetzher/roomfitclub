94b32b07919c5af34f9693491388f7e63b94311a
Zetzher <j.abasolo.agenjo@gmail.com>
2020-06-26 11:08:47 +0200

    done

--- servidor/app.js
+++ servidor/app.js
@@ -50,13 +50,13 @@ app.use(
 app.use(
   cors({
     credentials: true,
-    origin: ['http://localhost:3000', 'https://roomfitclub.web.app', 'https://roomfitclub.firebaseapp.com'],
+    origin: ['*'],
   })
 );
 
 
 app.use((req, res, next) => {
-     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000', 'https://roomfitclub.web.app', 'https://roomfitclub.firebaseapp.com');
+     res.setHeader('Access-Control-Allow-Origin', '*');
      res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS, DELETE');
      res.setHeader('Access-Control-Allow-Headers', 'origin, X-Requested-With, Content-Type, Accept');
      res.setHeader('Access-Control-Allow-Credentials', true);
