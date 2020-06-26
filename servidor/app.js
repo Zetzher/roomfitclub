de4c1239f4df006991878c1c8e2a10e83190033d
Zetzher <j.abasolo.agenjo@gmail.com>
2020-06-26 11:23:45 +0200

    done

--- servidor/app.js
+++ servidor/app.js
@@ -56,7 +56,7 @@ app.use(
 
 
 app.use((req, res, next) => {
-     res.setHeader('Access-Control-Allow-Origin', '*');
+     res.setHeader('Access-Control-Allow-Origin', 'https://roomfitclub.web.app', 'https://roomfitclub.firebaseapp.com');
      res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST OPTIONS, DELETE');
      res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
      res.setHeader('Access-Control-Allow-Credentials', true);
